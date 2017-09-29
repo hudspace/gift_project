73066819ce035d387742f869885c11c5fa50dd17
Brian Hudson <hudson_ops@yahoo.com>
2017-09-29 12:21:48 -0400

    Fixed bug in the app templates

--- gift_project/gift_project/settings.py
+++ gift_project/gift_project/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'gift_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['gift_project/templates',],
+        'DIRS': ['templates',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- gift_project/idea_tracker/views.py
+++ gift_project/idea_tracker/views.py
@@ -5,7 +5,7 @@ from .models import Recipient, Gift
 
 
 def forms(request):
-    return render(request, 'idea_tracker/forms.html')
+    return render(request, 'idea_tracker/forms.html', {'forms': forms})
 
 def shoppinglist(request):
     return render(request, 'idea_tracker/shoppinglist.html')
