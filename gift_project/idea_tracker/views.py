729f9970fa9cf59b6d198b72e90a1822469c9e1c
Brian Hudson <hudson_ops@yahoo.com>
2017-10-14 09:53:39 -0400

    Fixed bug causing perpetual display of 'this field is required' message on every field in gift form.

--- gift_project/idea_tracker/views.py
+++ gift_project/idea_tracker/views.py
@@ -8,21 +8,27 @@ from . import models
 
 
 def recipient_form(request):
-    form = forms.RecipientForm(request.POST)
+    form = forms.RecipientForm(request.POST or None)
     if form.is_valid():
         new_list = form.save(commit=False)
         new_list.save()
         messages.add_message(request, messages.SUCCESS, 'New gift recipient added!')
         return HttpResponseRedirect('/idea_tracker/forms/recipient_form')
+    else:
+        form = forms.RecipientForm()
     return render(request, 'idea_tracker/recipient_form.html', {'form': form})
 
 
 def gift_form(request):
-    form = forms.GiftForm(request.POST)
+    form = forms.GiftForm(request.POST or None)
+    print(form)
     if form.is_valid():
         new_item = form.save(commit=False)
         new_item.save()
         messages.add_message(request, messages.SUCCESS, 'New gift idea added!')
+        return HttpResponseRedirect('/idea_tracker/forms/gift_form')
+    else:
+        form = forms.GiftForm()
     return render (request, 'idea_tracker/gift_form.html', {'form': form})
 
 
