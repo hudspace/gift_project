a46977a0f522a82b2dca78f2e5d9317117968938
Brian Hudson <hudson_ops@yahoo.com>
2017-10-15 17:09:53 -0400

    Updated shopping_list view

--- gift_project/idea_tracker/models.py
+++ gift_project/idea_tracker/models.py
@@ -6,11 +6,14 @@ from django.db import models
 class Recipient(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    birthday = models.CharField(max_length=10)
+    birthday = models.CharField(max_length=10, blank=True)
     notes = models.TextField(max_length=255, blank=True)
 
     def __str__(self):
         return "{} {}".format(self.first_name, self.last_name)
+
+    def get_absolute_url(self):
+        return reverse('views.recipient_detail', args=[str(self.id)])
 
 
 class Gift(models.Model):
--- gift_project/idea_tracker/views.py
+++ gift_project/idea_tracker/views.py
@@ -1,4 +1,5 @@
 from django.contrib import messages
+from django.core.urlresolvers import reverse
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponseRedirect
 
@@ -10,33 +11,35 @@ from . import models
 def recipient_form(request):
     form = forms.RecipientForm(request.POST or None)
     if form.is_valid():
-        new_list = form.save(commit=False)
-        new_list.save()
+        instance = form.save(commit=False)
+        instance.save()
         messages.add_message(request, messages.SUCCESS, 'New gift recipient added!')
         return HttpResponseRedirect('/idea_tracker/forms/recipient_form')
     else:
         form = forms.RecipientForm()
-    return render(request, 'idea_tracker/recipient_form.html', {'form': form})
+    return render(request, 'idea_tracker/recipient_form.html', {'recipient_form': form})
 
 
 def gift_form(request):
     form = forms.GiftForm(request.POST or None)
     print(form)
     if form.is_valid():
-        new_item = form.save(commit=False)
-        new_item.save()
+        instance = form.save(commit=False)
+        instance.save()
         messages.add_message(request, messages.SUCCESS, 'New gift idea added!')
         return HttpResponseRedirect('/idea_tracker/forms/gift_form')
     else:
         form = forms.GiftForm()
-    return render (request, 'idea_tracker/gift_form.html', {'form': form})
+    return render (request, 'idea_tracker/gift_form.html', {'gift_form': form})
 
 
-def shopping_list(request):
-    recipients = models.Recipient.objects.order_by('first_name')
-    gifts = models.Gift.objects.order_by('name')
-    return render(request, 'idea_tracker/shoppinglist.html', {'recipients': recipients, 'gifts': gifts})
+def shopping_list(request, first_name):
+    recipients = models.Recipient.objects.filter(first_name=first_name)
+    return render(request, 'idea_tracker/shoppinglist.html', {'recipients': recipients, 'gifts': gifts, 'shoppingList': shoppingList})
 
-def recipient_detail(request):
-    recipient = models.Recipient.objects.order_by('first_name')
-    return render(request, 'idea_tracker/recipient_detail.html', {'recipient': recipient})
+def recipient_detail(request, first_name):
+    recipients = models.Recipient.objects.filter(first_name=first_name)
+    return render(request, 'idea_tracker/recipient_detail.html', {'first_name': first_name})
+
+def recipient_edit(request):
+    recipient = get_object_or_404(models.Recipient)
