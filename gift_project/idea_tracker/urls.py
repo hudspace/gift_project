84122574a5e868bc7a48049e2466a02743e28450
Brian Hudson <hudson_ops@yahoo.com>
2017-10-28 17:56:01 -0400

    Added normalize.css and some media queries.

--- gift_project/idea_tracker/urls.py
+++ gift_project/idea_tracker/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     url(r'^forms/gift_form/', views.gift_form, name='gift_form'),
     url(r'^shoppinglist/', views.shopping_list, name='shopping_list'),
     url(r'^recipient_update/(?P<pk>\d+)/$', views.RecipientUpdate.as_view(), name='recipient_update_form'),
+    url(r'^recipient_update/(?P<pk>\d+)/$', views.RecipientDelete.as_view()),
 ]
 
