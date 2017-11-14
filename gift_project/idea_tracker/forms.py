d6465b12c6dfb468af29325f63fc3dbb69d67f80
Brian Hudson <hudson_ops@yahoo.com>
2017-11-13 21:14:46 -0500

    Added widget to recipient_form and help text to gift_form.

--- gift_project/idea_tracker/forms.py
+++ gift_project/idea_tracker/forms.py
@@ -9,11 +9,17 @@ from . import models
 class RecipientForm(ModelForm):
     class Meta:
         model = models.Recipient
+        widgets = {
+            'birthday': forms.TextInput(attrs={'placeholder': 'ex: 11/17/2017'}),
+        }
         fields = ['first_name', 'last_name', 'birthday', 'notes',]
 
 class GiftForm(ModelForm):
     class Meta:
         model = models.Gift
+        help_texts = {
+            'recipients': 'Click on an intended recipient',
+        }
         fields = ['name', 'model_number', 'price', 'recipients', 'purchased']
 
 
